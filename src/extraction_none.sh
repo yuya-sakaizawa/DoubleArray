fa197be4e21a1ef890ecb8dcc00d41885303fccf
yuya-sakaizawa <ksf.doingmorewithless@gmail.com>
2015-03-31 14:16:20 +0900

    Delete extraction_none.sh

--- src/extraction_none.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-echo "抽出したい語を入力してください"
-read INPUT
-
-python entry_keyword.py -i $INPUT -o ../data/user.csv
-bash create_user_dic.sh ../data/user.csv ~/Downloads/unidic-mecab-2.1.2_src/ user.dic
-python extraction_none.py
-
